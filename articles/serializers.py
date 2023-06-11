8981b202b5013b9e6c26fbdedf6ad8f2fe551d27
Shakhbozbek Usmonov <83509440+shakhbozbekusmonov@users.noreply.github.com>
2023-06-11 23:34:24 +0500

    deta sh deploy

--- articles/serializers.py
+++ articles/serializers.py
@@ -10,6 +10,7 @@ class CategorySerializer(serializers.ModelSerializer):
 
 
 class ArticlesSerializer(serializers.ModelSerializer):
+    image = serializers.ImageField(max_length=None, use_url=True)
 
     class Meta:
         model = Articles
--- config/urls.py
+++ config/urls.py
@@ -29,4 +29,7 @@ urlpatterns = [
     path('api/v1/auth/registration/', include('dj_rest_auth.registration.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
